b4ac96550de5a3e08a79525ce223e74baa1fd96d
Piotr Główka <piotr@glowkapracuje.pl>
2020-03-27 16:43:45 +0100

    Showing or hiding admin links

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
-import 'rxjs/add/operator/switchMap';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -11,10 +10,8 @@ export class AdminAuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean> {
-    return this.auth.user$
-      .switchMap(user => this.userService.get(user.uid))
+    return this.auth.appUser$
       .map(appUser => appUser.isAdmin);
   }
-  // map & switch to a new observable
 
 }
