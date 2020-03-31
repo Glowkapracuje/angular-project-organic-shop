3ebaf21156d8773ac833a65aa04b30de391c071e
Piotr Główka <piotr@glowkapracuje.pl>
2020-03-31 19:17:58 +0200

    Fixing bug with refreshing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,18 @@ export class AppComponent {
 
   constructor(private auth: AuthService, router: Router, private userService: UserService) {
     auth.user$.subscribe(user => {
-      if (user) {
-        userService.save(user);
+      if (!user) return;
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
-      }
-    })
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+
+      if (!returnUrl) return;
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
+
+    });
   }
 
 }
