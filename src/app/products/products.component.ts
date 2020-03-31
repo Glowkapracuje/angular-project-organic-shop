486b212c2df5b1e327c89a316e80c729fdd50f01
Piotr Główka <piotr@glowkapracuje.pl>
2020-03-31 20:31:39 +0200

    Set products page as home & display products and categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
+import { CategoryService } from 'app/category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
+  categories$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(productService: ProductService, categoryService: CategoryService) {
+    this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
   }
 
 }
