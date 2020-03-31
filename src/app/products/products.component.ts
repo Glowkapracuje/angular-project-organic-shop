43150f480bf79edaa9fc039184e8c4b35130cc0b
Piotr Główka <piotr@glowkapracuje.pl>
2020-03-31 20:51:37 +0200

    Filter products by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'app/product.service';
 import { CategoryService } from 'app/category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'app/models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,12 +10,25 @@ import { CategoryService } from 'app/category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+    productService.getAll().subscribe(products => this.products = products)
+
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) : this.products;
+    });
   }
 
 }
