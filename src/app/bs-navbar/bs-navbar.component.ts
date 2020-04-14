7e3412edd2fa4c6df8ceee4fac8e4918687ba6a2
Piotr Główka <piotr@glowkapracuje.pl>
2020-04-14 20:00:36 +0200

    Adding badge with items count in shopping cart

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/auth.service';
 import { AppUser } from 'app/models/app-user';
+import { ShoppingCartService } from 'app/shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  shoppingCartItemCount: number;
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser);
-   }
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
+
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+
+    let cart$ = await this.shoppingCartService.getCart()
+    // We have not to unsubscribe, because we will dealing with single instance of navbar component in DOM
+    // with this subscription below logic will be reexecuted with every change
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity
+      }
+    });
+  }
 
   logout() {
     this.auth.logout();
