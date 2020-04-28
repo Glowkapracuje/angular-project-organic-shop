cbff7464c84cf4f7f6a6e388c906a565d0f143db
Piotr Główka <piotr@glowkapracuje.pl>
2020-04-28 23:31:41 +0200

    Creating Admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { DataTableModule } from 'angular-4-data-table/src';
+import { SharedModule } from 'shared/shared.module';
 
 import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './components/admin-products/admin-products.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { AdminAuthGuardService } from './services/admin-auth-guard.service';
-import { FormsModule } from '@angular/forms';
-import { SharedModule } from 'shared/shared.module';
-import { DataTableModule } from 'angular-4-data-table/src';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 @NgModule({
@@ -15,7 +17,31 @@ import { DataTableModule } from 'angular-4-data-table/src';
     CommonModule,
     FormsModule,
     SharedModule,
-    DataTableModule
+    DataTableModule,
+    RouterModule.forChild([
+      {
+        path: 'admin/products/new',
+        component: ProductFormComponent,
+        canActivate: [AuthGuardService, AdminAuthGuardService]
+      },
+      {
+        path: 'admin/products/:id',
+        component: ProductFormComponent,
+        canActivate: [AuthGuardService, AdminAuthGuardService]
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+        canActivate: [AuthGuardService, AdminAuthGuardService]
+      },
+
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent,
+        canActivate: [AuthGuardService, AdminAuthGuardService]
+      }
+
+    ])
   ],
   declarations: [
     AdminProductsComponent,
